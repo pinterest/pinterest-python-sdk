bdb4cc6227300250efa33ad9f9e9e2470def8744
Gilderlane Ribeiro Alexandre <gribeiroalexandre@pinterest.com>
2023-10-02 15:35:13 +0000

    Fix valition of responses

--- pinterest/utils/analytics.py
+++ pinterest/utils/analytics.py
@@ -39,7 +39,7 @@ class AnalyticsUtils:
         return AnalyticsResponse(
             entity_type=entity,
             fields=params.get('columns', []),
-            raw_response=getattr(api(client), analytics_fn)(**params, **kwargs)
+            raw_response=getattr(api(client), analytics_fn.__name__)(**params, **kwargs)
         )
 
 
@@ -56,11 +56,11 @@ class AnalyticsResponse():
         """
         Initialize an Ads Analytics object.
         Args:
-            entity_type (str): Entity Type identifier. Enum: ad_account, campaign, ad_group, ad.
+            entity_type (PinterestBaseModel): Entity Type identifier. Enum: ad_account, campaign, ad_group, ad.
             fields (list[str]): _description_
             raw_response (dict): _description_
         """
-        self._entity_type = AdsEntityType(entity_type).name
+        self._entity_type = entity_type.__name__.lower()
         self._fields = fields
         self._raw_response = raw_response
 
