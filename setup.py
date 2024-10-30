97f54d66510f68f0e7fe56124f277518ccafdd5b
Bharadwaj P <t20@users.noreply.github.com>
2024-10-30 12:59:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ _IS_TEST_BUILD = os.environ.get("IS_TEST_BUILD", 0)
 REQUIRES = [
   "urllib3==1.26.12",
   "python-dateutil",
-  "python-dotenv==0.20.0",
+  "python-dotenv>=0.20.0",
   "six==1.16.0",
   "Pinterest-Generated-Client==0.1.9"
 ]
